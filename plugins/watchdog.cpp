c96b49e0535eab518390eb9527b58950d763e282
Mohammed Sameer <msameer@foolab.org>
2015-12-06 02:05:43 +0200

    Rework watchdog timer.
    Use a single timer that triggers every 10 second to kick both watchdogs.
    This is what dsme does more or less.

--- plugins/watchdog.cpp
+++ plugins/watchdog.cpp
@@ -1,6 +1,5 @@
 #include "plugin.h"
-#include <map>
-#include <map>
+#include <list>
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
@@ -11,6 +10,8 @@
 #include <sys/ioctl.h>
 #include <linux/watchdog.h>
 
+#define TIMEOUT 10
+
 std::map<std::string, int> devices = {
   {"/dev/watchdog", 20},
   {"/dev/twl4030_wdt", 30},
@@ -18,16 +19,15 @@ std::map<std::string, int> devices = {
 
 void kick_wd(boost::asio::deadline_timer *t, int& fd, int time)
 {
-  if (write(fd, "*", 1) != 1) {
-    std::cerr << "Failed to kick watchdog: " << std::strerror(errno) << std::endl;
-  }
-
   t->expires_at(t->expires_at() + boost::posix_time::seconds(time));
   t->async_wait(boost::bind(kick_wd, t, fd, time));
 }
 
 class WatchdogPlugin : public Plugin {
 public:
+  WatchdogPlugin() :
+    m_timer(nullptr) { }
+
   bool start(boost::asio::io_service& service) {
     for (auto& wd : devices) {
       int fd = open(wd.first.c_str(), O_RDWR);
@@ -36,30 +36,56 @@ public:
 	continue;
       }
 
+      // We don't care if it fails
       int tmp = wd.second;
       ioctl(fd, WDIOC_SETTIMEOUT, &tmp);
 
-      boost::asio::deadline_timer
-	*t(new boost::asio::deadline_timer(service, boost::posix_time::seconds(wd.second)));
-
-      kick_wd(t, fd, wd.second);
-
-      m_timers.insert(std::make_pair(fd, t));
+      m_fds.push_back(fd);
     }
+
+    if (m_fds.size() > 0) {
+      m_timer = new boost::asio::deadline_timer(service, boost::posix_time::seconds(TIMEOUT));
+      tick(true);
+    }
+
+    return m_fds.size() > 0;
   }
 
   void stop() {
-    for (auto& wd : m_timers) {
-      wd.second->cancel();
-      delete wd.second;
-      close(wd.first);
+    for (int fd : m_fds) {
+      close(fd);
     }
 
-    m_timers.clear();
+    m_fds.clear();
+    if (m_timer) {
+      m_timer->cancel();
+      delete m_timer;
+      m_timer = nullptr;
+    }
   }
 
 private:
-  std::map<int, boost::asio::deadline_timer *> m_timers;
+  void tick(bool initial_tick) {
+    for (int fd : m_fds) {
+      kick_wd(fd);
+    }
+
+    if (!initial_tick) {
+      // Reschedule timer.
+      m_timer->expires_at(m_timer->expires_at() + boost::posix_time::seconds(TIMEOUT));
+    }
+
+    m_timer->async_wait(boost::bind(&WatchdogPlugin::tick, this, false));
+  }
+
+  void kick_wd(int fd) {
+    if (write(fd, "*", 1) != 1) {
+      std::cerr << "Failed to kick watchdog: " << std::strerror(errno) << std::endl;
+    }
+  }
+
+  boost::asio::deadline_timer *m_timer;
+  std::list<int> m_fds;
 };
 
 REGISTER_PLUGIN(WatchdogPlugin)
